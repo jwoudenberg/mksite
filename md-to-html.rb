d8be2718c2789bf12dfbc1723aac65d848033c55
Jasper Woudenberg <mail@jasperwoudenberg.com>
2022-08-27 16:39:49 +0200

    Add a markdown to html converstion script
    This can be used as a filter to the mksite command.

---
+++ md-to-html.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i ruby -p "ruby.withPackages (ps: [ps.redcarpet])"
+
+require "redcarpet"
+require "yaml"
+require "cgi"
+
+contents = $stdin.read
+metadata = {}
+if contents.start_with?("---")
+  _, frontmatter, contents = contents.split("---")
+  metadata = YAML.safe_load(frontmatter)
+end
+markdown = Redcarpet::Markdown.new(Redcarpet::Render::XHTML, extensions = {})
+body = markdown.render(contents)
+
+def puts_meta(key, value)
+  case value
+    when String
+      puts %(<meta name="#{key}" content="#{CGI.escapeHTML(value)}" />)
+    when Array
+      value.each { |item| puts_meta(key, item) }
+  end
+end
+
+puts "<!doctype>"
+puts "<html>"
+puts "<head>"
+metadata.each { |key, value| puts_meta(key, value) }
+puts "</head>"
+puts "<body>"
+puts body
+puts "</body>"
+puts "</html"
